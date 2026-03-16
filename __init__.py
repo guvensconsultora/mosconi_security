4488109fcc27b69ef99a3bb0d1024956afaa6fe0
guvensconsultora <guvensconsultora@gmail.com>
2026-03-16 08:25:50 -0300

    init: módulo mosconi_security

---
+++ __init__.py
@@ -0,0 +1,1 @@
+# Módulo de seguridad: no requiere modelos Python
---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Mosconi - Seguridad y Accesos',
+    'version': '1.0.0',
+    'category': 'Sales',
+    'summary': 'Record rules para restringir pedidos por sitio web',
+    'description': 'Restringe usuarios del grupo Ventas Crazycompras a ver solo pedidos del website_id=1',
+    'depends': ['sale', 'website_sale', 'payment', 'mail', 'base_automation'],
+    'data': [
+        'security/groups.xml',
+        'security/ir.model.access.csv',
+        'security/rules.xml',
+        'data/mail_templates.xml',
+        'data/automation.xml',
+        'views/menus.xml',
+    ],
+    'installable': True,
+    'license': 'LGPL-3',
+}
